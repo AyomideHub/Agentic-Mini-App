85deb29180622f8ebb3a7ed079de08ac47510366
AyomideHub <dadaayomide64@gmail.com>
2025-07-25 08:16:01 +0100

    helmet gone

--- server.js
+++ server.js
@@ -3,22 +3,22 @@ import dotenv from "dotenv";
 import controller from "./backend/controller.js";
 dotenv.config();
 import cors from "cors";
-import helmet from "helmet";  
+// import helmet from "helmet";  
 
 // Initialize Express app
 const app = express();
 
 app.use(cors()); // Enable CORS for all routes
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-        "frame-ancestors": ["'self'", "https://yakihonne.com", "https://yakihonne.com/sw-playground"], // For testing, allow all. For production, restrict as needed.
-      },
-    },
-  })
-);
+// app.use(
+//   helmet({
+//     contentSecurityPolicy: {
+//       directives: {
+//         ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+//         "frame-ancestors": ["'self'", "https://yakihonne.com", "https://yakihonne.com/sw-playground"], // For testing, allow all. For production, restrict as needed.
+//       },
+//     },
+//   })
+// );
 // Middleware to parse JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
