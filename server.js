1320a4125be767c3103abeef3f12ad2b7cca196a
AyomideHub <dadaayomide64@gmail.com>
2025-07-25 08:07:13 +0100

    removed helmet

--- server.js
+++ server.js
@@ -3,28 +3,35 @@ import dotenv from "dotenv";
 import controller from "./backend/controller.js";
 dotenv.config();
 import cors from "cors";
-import helmet from "helmet";  
+// import helmet from "helmet";
 
 // Initialize Express app
 const app = express();
 
 app.use(cors()); // Enable CORS for all routes
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-        "frame-ancestors": ["'self'", "https://yakihonne.com", "https://yakihonne.com/sw-playground"], // For testing, allow all. For production, restrict as needed.
-      },
-    },
-  })
-);
+// app.use(
+//   helmet({
+//     contentSecurityPolicy: {
+//       directives: {
+//         ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+//         "frame-ancestors": [
+//           "'self'",
+//           "https://yakihonne.com",
+//           "https://yakihonne.com/sw-playground",
+//           "https://playground.yakihonne.com",
+//         ],
+//         "script-src": ["'self'", "https://unpkg.com"],
+//       },
+//     },
+//   })
+// );
+
 // Middleware to parse JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Serve static files from the "public" directory
-app.use('/.well-known', express.static('public/.well-known'));
+app.use("/.well-known", express.static("public/.well-known"));
 app.use(express.static("public"));
 
 // Set up the translation endpoint
